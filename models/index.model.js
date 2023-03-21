268da8abfde55ff407c6987036a54f6e4bf6d94a
iznakhurshid25 <f200182@cfd.nu.edu.pk>
2023-03-22 02:29:03 +0500

    Order Model Created

--- models/index.model.js
+++ models/index.model.js
@@ -4,6 +4,8 @@ db.Customers = require("./customer.model")(db.sequelize,db.Sequelize);
 db.category = require("./category.model")(db.sequelize,db.Sequelize);
 db.subCategory = require("./sub.category.model")(db.sequelize,db.Sequelize);
 
+db.orders = require("./order.model")(db.sequelize,db.Sequelize);
+// Associations 
 db.category.hasMany(db.subCategory, {foreignKey: 'Cat_ID'});
 db.subCategory.belongsTo(db.category, {foreignKey: 'Cat_ID'});
 
