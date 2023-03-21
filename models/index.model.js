f6960a84bc8c87f8468142982d0979f176b006aa
iznakhurshid25 <f200182@cfd.nu.edu.pk>
2023-03-22 02:40:09 +0500

    Payment Model Created

--- models/index.model.js
+++ models/index.model.js
@@ -6,6 +6,7 @@ db.subCategory = require("./sub.category.model")(db.sequelize,db.Sequelize);
 
 db.orders = require("./order.model")(db.sequelize,db.Sequelize);
 db.ordersItems = require("./order.items.model")(db.sequelize,db.Sequelize);
+db.Payments = require("./payment.model")(db.sequelize,db.Sequelize);
 // Associations 
 db.category.hasMany(db.subCategory, {foreignKey: 'Cat_ID'});
 db.subCategory.belongsTo(db.category, {foreignKey: 'Cat_ID'});
