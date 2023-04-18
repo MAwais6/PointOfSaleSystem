f240d27fb4a068d3dbe9346bb2d4e60e8b7395a1
MAwais6 <f200265@cfd.nu.edu.pk>
2023-04-18 14:22:28 +0500

    Issue are fixed

--- controller/discounts.controller.js
+++ controller/discounts.controller.js
@@ -1,127 +1,131 @@
 const db = require("../models/index.model.js");
-const Discounts = db.Discount;
+const Discounts = db.discount;
 const Op = db.Sequelize.Op;
+
+// Create and Save a new Discount
 exports.create = (req, res) => {
+  // Validate request
   if (!req.body.StartDate) {
     res.status(400).send({
-      message: "Content can not be empty!",
+      message: "Content can not be empty!"
     });
     return;
   }
+
+  // Create a Discount
   const discount = {
-    StartDate: req.body.StartDate,
-    EndDate: req.body.EndDate
+    StartDate: req.body.DISCOUNT_START_DATE,
+    EndDate: req.body.DISCOUNT_END_DATE,
   };
+
+  // Save Discount in the database
   Discounts.create(discount)
-    .then((data) => {
+    .then(data => {
       res.send(data);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
         message:
-          "Some error occurred while creating the Discount.",
+          err.message || "Some error occurred while creating the Discount."
       });
     });
-};
+}
+
+// Retrieve all Discounts from the database.
 exports.findAll = (req, res) => {
-    if(req.query.page && req.query.pageSize){
-      page = req.query.page
-      pageSize = req.query.pageSize
-      condition = { offset:(page*1-1)*pageSize, limit:pageSize*1 }
-    }
-    else{
-      EndDate = req.query.EndDate;
-      condition = EndDate ? {where: { EndDate: { [Op.lte]: `%${EndDate}%` } } } : {}
-    }
-    console.log(condition)
-    Discounts.findAll(condition)
-      .then((data) => {
-        res.send(data)
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving Products.",
-        })
-      })
-};
-exports.findOne = (req, res) => {
-  const D_ID = req.params.D_ID;
-  Discounts.findByPk(D_ID)
-    .then((data) => {
-      if (data) {
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Cannot find Discount with id=${D_ID}.`,
-        });
-      }
+  const DISCOUNT_CODE = req.query.DISCOUNT_CODE;
+  var condition = DISCOUNT_CODE ? { DISCOUNT_CODE: { [Op.like]: `%${DISCOUNT_CODE}%` } } : null;
+
+  Discounts.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
-        message: "Error retrieving Discount with id=" + D_ID,
+        message:
+          err.message || "Some error occurred while retrieving discounts."
       });
     });
-};
+}
+
+// Find a single Discount with an id
+exports.findOne = (req, res) => {
+  const id = req.params.D_ID;
+
+  Discounts.findByPk(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Discount with id=" + id
+      });
+    });
+}
+
+// Update a Discount by the id in the request
 exports.update = (req, res) => {
   const D_ID = req.params.D_ID;
+
   Discounts.update(req.body, {
-    where: { D_ID: D_ID },
+    where: { D_ID: D_ID }
   })
-    .then((num) => {
+    .then(num => {
       if (num == 1) {
         res.send({
-          message: "Discount was updated successfully.",
+          message: "Discount was updated successfully."
         });
       } else {
         res.send({
-          message: `Cannot update Discount with id=${D_ID}. Maybe Discount was not found or req.body is empty!`,
+          message: `Cannot update Discount with id=${D_ID}. Maybe Discount was not found or req.body is empty!`
         });
       }
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
-        message: "Error updating Discount with id=" + D_ID,
+        message: "Error updating Discount with id=" + D_ID
       });
     });
-};
+}
 
-// Delete a Tutorial with the specified id in the request
+// Delete a Discount with the specified id in the request
 exports.delete = (req, res) => {
   const D_ID = req.params.D_ID;
 
   Discounts.destroy({
-    where: { D_ID: D_ID },
+    where: { D_ID: D_ID }
   })
-    .then((num) => {
+    .then(num => {
       if (num == 1) {
         res.send({
-          message: "Discount was deleted successfully!",
+          message: "Discount was deleted successfully!"
         });
       } else {
         res.send({
-          message: `Cannot delete Discount with id=${D_ID}. Maybe Discount was not found!`,
+          message: `Cannot delete Discount with id=${D_ID}. Maybe Discount was not found!`
         });
       }
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
-        message: "Could not delete Discount with id=" + D_ID,
+        message: "Could not delete Discount with id=" + D_ID
       });
     });
-};
+}
+
+// Delete all Discounts from the database.
 exports.deleteAll = (req, res) => {
-    Discounts.destroy({
+  Discounts.destroy({
     where: {},
-    truncate: false,
+    truncate: false
   })
-    .then((nums) => {
+    .then(nums => {
       res.send({ message: `${nums} Discounts were deleted successfully!` });
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while removing all Discounts.",
+          err.message || "Some error occurred while removing all discounts."
       });
     });
-};
+}
--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -8,6 +8,8 @@ const productRoutes = require("./products.routes.js")
 const categoryRoutes = require("./category.routes.js")
 const subCategoryRoutes = require("./subcategory.routes.js")
 const orderRoutes = require("./order.routes.js")
+const discountRoutes = require("./discounts.routes.js")
+const discountItemRoutes = require("./discountItems.routes.js")
 
 // use the routes
 router.get("/dashboard",[authJwt.verifyToken] ,controller.customerDashboard)
@@ -16,6 +18,8 @@ router.use("/products", [authJwt.verifyToken] , productRoutes)
 router.use("/category", [authJwt.verifyToken] , categoryRoutes)
 router.use("/subcategory", [authJwt.verifyToken] , subCategoryRoutes)
 router.use("/order", [authJwt.verifyToken] , orderRoutes)
+router.use("/discount", [authJwt.verifyToken] , discountRoutes)
+router.use("/discountItem", [authJwt.verifyToken] , discountItemRoutes)
 
 // export the router
 module.exports = router;
