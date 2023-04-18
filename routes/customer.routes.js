c2cf785b690c62c1a8092bae638ddf21d656ed7d
MAwais6 <f200265@cfd.nu.edu.pk>
2023-04-18 10:23:46 +0500

    API testing Category Done

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -5,11 +5,13 @@ const router = express.Router()
 
 // require the routes
 const productRoutes = require("./products.routes.js")
+const categoryRoutes = require("./category.routes.js")
 
 // use the routes
 router.get("/dashboard",[authJwt.verifyToken] ,controller.customerDashboard)
 
 router.use("/products", [authJwt.verifyToken] , productRoutes)
+router.use("/category", [authJwt.verifyToken] , categoryRoutes)
 
 // export the router
 module.exports = router;
